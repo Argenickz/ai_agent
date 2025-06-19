bc4c42022ed4d47e867c09cef616668f78b28433
Argenickz <argenickz@gmail.com>
2025-06-18 22:06:18 -0400

    june 18/25

--- functions/write_files.py
+++ functions/write_files.py
@@ -8,36 +8,37 @@ Up until now our program has been read-only. We'll give our agent the ability to
 def write_file(working_directory, file_path, content):
     # get a hold of the working directory absolute path
     working_directory_absolute_path = os.path.abspath(working_directory)
-    print(f"this is the working difectory absolute path:\n{working_directory_absolute_path}\n")
 
     # get a hold of the file_path working directory
     file_path_absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
-    print(f"this is the file_path absolute path:\n{file_path_absolute_path}\n")
 
     if not file_path_absolute_path.startswith(working_directory_absolute_path):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     try:
         # !If the file_path doesn't exist, create it. As always if there are any errors return a string representing the error, prefixed with 'Error:'.
-        # If the file_path doesn't exist create it
-        if not os.path.exists(file_path_absolute_path):
-            print('the filepath doesnt exist create it\n')
-            filename = os.path.basename(file_path_absolute_path)
-            print(f"this is the filename: {filename}")
-            # todo. Using dirname and os.path.base name I can get the directory name and the file name separately, even if they don't exist. if the filepath (using dirname) doesn't exits, create it. then with open(filename 'w') to write/rewrite to the file in said directory.
+        # Isolate the filepath
+        new_filepath = os.path.dirname(file_path_absolute_path)
 
+        # If the filepath doesn't exist create it
+        if not os.path.exists(new_filepath):
+            # Create the new directory(ies)
+            os.makedirs(new_filepath)
+            # Write to the file once the directory has ben created
+            with open(file_path_absolute_path, "w") as file:
+                file.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        else:
+            # Else just write to the directory that already exist
+            with open(file_path_absolute_path, "w") as file:
+                file.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
 
     except Exception as e:
         return f'Error processing file {file_path}\n{e}'
     
 
-    
-        
-
-
-
-
 def main():
-    result = write_file("calculator", "pkg/more_shit.txt", "hey I just met you, and this is craaazy!")
+    result = write_file("calculator", "/temp/temp.txt", "this should not be allowed")
     print(result)
 if __name__ == "__main__":
     main()
--- tests.py
+++ tests.py
@@ -1,18 +1,31 @@
 from functions.get_files_info import get_files_info
 from functions.get_files_content import get_file_content
+from functions.write_files import write_file
 import unittest
 
 class TestGetFilesInfo(unittest.TestCase):
-    def test_files(self):
-        report = get_file_content("calculator", "main.py")
-        print(report)
+    def test_one(self):
+        result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+        print(result)
+    
+    def test_two(self):
+        result = write_file("calculator", "pgk/morelorem.txt", "lorem ipsum dolor sit amet")
+        print(result)
 
-    def test_files2(self):
-        report2 = get_file_content("calculator", "pkg/calculator.py")
-        print(report2)
-    def test_files3(self):
-        report3 = get_file_content("calculator", "/bin/cat")
-        print(report3)
+    def test_three(self):
+        result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+        print(result)
+
+    # def test_files(self):
+    #     report = get_file_content("calculator", "main.py")
+    #     print(report)
+
+    # def test_files2(self):
+    #     report2 = get_file_content("calculator", "pkg/calculator.py")
+    #     print(report2)
+    # def test_files3(self):
+    #     report3 = get_file_content("calculator", "/bin/cat")
+    #     print(report3)
     # def test_one(self):
     #     report = get_files_info("calculator", ".")
     #     print(report)
